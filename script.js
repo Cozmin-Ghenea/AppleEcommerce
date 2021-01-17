244583eaa260eedd6937553ef5b813dcd9878bf2
Cozmin-Ghenea <43807754+Cozmin-Ghenea@users.noreply.github.com>
2021-01-17 15:48:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const slideshowDivs = () => {
     for (let i = 1; i <= 5; i++) {
         const div = document.createElement('div')
 
-        div.style.backgroundImage = `url(/images/slideshow/section-1-bg-${i}.jpg)`
+        div.style.backgroundImage = `url(./images/slideshow/section-1-bg-${i}.jpg)`
 
         i === 1 && div.classList.add('change')
 
